7577c889bbcc1836441132f96f5daa93fa0b28a1
fscherwi <Florian@Scherwitzls-iMac.local>
2015-09-05 20:49:51 +0200

    publish with grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,22 +2,42 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
     replace: {
-      another_example: {
-        src: ['*js'],
-        dest: 'build/',
-        //overwrite: true, // overwrite matched source files
+      coverage: {
+        src: ['*.js', '!Gruntfile.js'],
+        overwrite: true,
         replacements: [{
           from: '/* istanbul ignore next */',
           to: ''
         }]
       }
+    },
+    shell: {
+      new_folder: {
+        command: 'mkdir coverage_files'
+      },
+      copy: {
+        command: 'cp *.js ./coverage_files'
+      },
+      replace_coverage: {
+        command: 'grunt replace_coverage_config'
+      },
+      publish: {
+        command: 'npm publish'
+      },
+      move_back: {
+        command: 'mv ./coverage_files/*.js ./'
+      },
+      remove_folder: {
+        command: 'rm -rf coverage_files'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['replace']);
+  grunt.registerTask('replace_coverage_config', ['replace']);
+  grunt.registerTask('publish', ['shell']);
 
 };
