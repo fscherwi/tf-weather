2117bfb0a1f06ff8f1d8f41c03f1289b15ace4a8
fscherwi <Florian@ScherwitzlsiMac.home>
2016-01-01 20:31:56 +0100

    move command.js to bin folder

--- bin/command.js
+++ bin/command.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-
 var program = require('commander');
-
 program
   .version(require('./package.json').version)
   .usage('[options]')
@@ -12,21 +10,19 @@ program
   .parse(process.argv);
 /* istanbul ignore next */
 if (!program.args.length) {
-  var w = require('./weather.js');
   if (!program.host) {
     program.host = 'localhost';
   }
   if (!program.port) {
     program.port = 4223;
   }
-
   if (program.live) {
     if (!program.wait) {
       program.wait = 1000;
     }
-    w.get(program.host, program.port, program.wait, live = true);
+    require('./weather.js').get(program.host, program.port, program.wait, live = true);
   } else {
-    w.get(program.host, program.port, live = false);
+    require('./weather.js').get(program.host, program.port, live = false);
   }
 } else {
   program.help();
