6bcdf0d413986c80fe7f76d1d894a79a30ce15e7
fscherwi <fscherwi@users.noreply.github.com>
2019-03-03 11:29:29 +0100

    Update output.js

--- src/output.js
+++ src/output.js
@@ -2,13 +2,11 @@ const logUpdate = require('log-update');
 const time = require('./time.js');
 
 module.exports.output = function (outputData) {
-	logUpdate(
-		`
+	logUpdate(`
 Relative Humidity: ${outputData[0]}
 Air pressure:      ${outputData[1]}
 Temperature:       ${outputData[2]}
 Illuminance:       ${outputData[3]}
 Time:              ${time.get()}
-`
-	);
+`);
 };
