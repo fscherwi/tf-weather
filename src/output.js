cd1f593d081c39a5714b1b64db5ab87c2879baf4
fscherwi <fscherwi@outlook.com>
2019-10-10 22:16:21 +0200

    improve output

--- src/output.js
+++ src/output.js
@@ -2,10 +2,7 @@ const logUpdate = require('log-update');
 
 module.exports.output = function (outputData) {
 	logUpdate(`
-${(outputData[0] ? ('Relative Humidity: ' + outputData[0]) : '')} %RH
-${(outputData[1] ? ('Air pressure:      ' + outputData[1]) : '')} mbar
-${(outputData[2] ? ('Temperature:       ' + outputData[2]) : '')} \u00B0C
-${(outputData[3] ? ('Illuminance:       ' + outputData[3]) : '')} Lux
+${(outputData[0] ? ('Relative Humidity: ' + outputData[0] + ' %RH\n') : '')}${(outputData[1] ? ('Air pressure:      ' + outputData[1] + ' mbar\n') : '')}${(outputData[2] ? ('Temperature:       ' + outputData[2] + ' \u00B0C\n') : '')}${(outputData[3] ? ('Illuminance:       ' + outputData[3] + ' Lux\n') : '')}
 Time:              ${time()}
 `);
 };
