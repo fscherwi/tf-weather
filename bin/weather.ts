d0bb9bf80b5d10b5f6a9741d5279c1ddce5eef8b
fscherwi <fscherwi@outlook.com>
2021-01-24 14:19:23 +0100

    improve input validation

--- bin/weather.ts
+++ bin/weather.ts
@@ -3,6 +3,7 @@
 import { createCommand } from 'commander';
 import { tfget } from '../src/get-weather';
 import { version } from '../package.json';
+const inRange = require('in-range');
 
 const program = createCommand();
 
@@ -10,19 +11,24 @@ program
 	.version(version)
 	.option('-l, --live', 'Shows the live weather')
 	.option('-h, --host [host]', 'The HOST, default to "localhost"')
-	.option('-p, --port [port]', 'The PORT, default to "4223"')
-	.option('-w, --wait [time]', 'The Callback time in milliseconds, default to "1000" ms')
+	.option('-p, --port [port]', 'The PORT, default to "4223"', parseInt)
+	.option('-w, --wait [time]', 'The Callback time in milliseconds, default to "1000" ms', parseInt)
 	.parse(process.argv);
 
 const options = program.opts();
 
-if (options.port < 0 || options.port >= 65536) {
-	console.error('\nPlease check your inserted PORT\n');
+if (typeof options.host !== ('string' && 'undefined')) {
+	console.error('Error: check your inserted HOST');
 	process.exit(1);
 }
 
-if (options.wait < 0 || options.wait > 4294967295) {
-	console.error('\nPlease check your inserted Callback time\n');
+if ((options.port != undefined && !Number.isInteger(options.port)) || (options.port != undefined && Number.isInteger(options.port) && !inRange(options.port, { start: 0, end: 65536 }))) {
+	console.error('Error: check your inserted PORT');
+	process.exit(1);
+}
+
+if ((options.wait != undefined && !Number.isInteger(options.wait)) || (options.wait != undefined && Number.isInteger(options.wait) && !inRange(options.wait, { start: 0, end: 4294967295 }))) {
+	console.error('Error: check your inserted Callback time');
 	process.exit(1);
 }
 
