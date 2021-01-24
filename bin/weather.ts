eb7d3627d4663b897966260a177f5998c0679557
fscherwi <fscherwi@outlook.com>
2021-01-24 14:29:49 +0100

    use yargs instead of commander

--- bin/weather.ts
+++ bin/weather.ts
@@ -1,37 +1,25 @@
 #!/usr/bin/env node
 
-import { createCommand } from 'commander';
+import yargs from 'yargs';
 import { tfget } from '../src/get-weather';
-import { version } from '../package.json';
-const inRange = require('in-range');
 
-const program = createCommand();
+const { argv } = yargs.options({
+	live: { type: 'boolean', default: false, description: 'Shows the live weather' },
+	host: { type: 'string', default: 'localhost', description: 'The HOST' },
+	port: { type: 'number', default: 4223, description: 'The PORT' },
+	wait: { type: 'number', default: 1000, description: 'The Callback time in ms' }
+});
 
-program
-	.version(version)
-	.option('-l, --live', 'Shows the live weather')
-	.option('-h, --host [host]', 'The HOST, default to "localhost"')
-	.option('-p, --port [port]', 'The PORT, default to "4223"', parseInt)
-	.option('-w, --wait [time]', 'The Callback time in milliseconds, default to "1000" ms', parseInt)
-	.parse(process.argv);
-
-const options = program.opts();
-
-if (typeof options.host !== ('string' && 'undefined')) {
-	console.error('Error: check your inserted HOST');
-	process.exit(1);
-}
-
-if ((options.port != undefined && !Number.isInteger(options.port)) || (options.port != undefined && Number.isInteger(options.port) && !inRange(options.port, { start: 0, end: 65536 }))) {
+if (argv.port > 65536) {
 	console.error('Error: check your inserted PORT');
 	process.exit(1);
 }
 
-if ((options.wait != undefined && !Number.isInteger(options.wait)) || (options.wait != undefined && Number.isInteger(options.wait) && !inRange(options.wait, { start: 0, end: 4294967295 }))) {
+if (argv.wait > 4294967295) {
 	console.error('Error: check your inserted Callback time');
 	process.exit(1);
 }
 
 (async () => {
-	await tfget(options.host || 'localhost', options.port || 4223, options.wait || 1000, options.live || false);
+	await tfget(argv.host, argv.port, argv.wait, argv.live);
 })();
